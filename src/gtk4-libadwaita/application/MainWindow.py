23bfb519326b5a3bd8630062da595fc368d53815
natorsc <natorsc@gmail.com>
2022-03-04 17:46:17 -0300

    Código inicial Adw.Application.

---
+++ src/gtk4-libadwaita/application/MainWindow.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Python e GTK 4: PyGObject Adw.Application."""
+
+import gi
+
+gi.require_version(namespace='Gtk', version='4.0')
+gi.require_version(namespace='Adw', version='1')
+
+from gi.repository import Gio, Gtk
+from gi.repository import Adw
+
+
+class MainWindow(Gtk.ApplicationWindow):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.set_title(title='Python e GTK 4: PyGObject Adw.Application')
+        # Tamanho inicial da janela.
+        self.set_default_size(width=int(1366 / 2), height=int(768 / 2))
+        # Tamanho minimo da janela.
+        self.set_size_request(width=int(1366 / 2), height=int(768 / 2))
+
+
+class Application(Adw.Application):
+
+    def __init__(self):
+        super().__init__(application_id='br.natorsc.Exemplo',
+                         flags=Gio.ApplicationFlags.FLAGS_NONE)
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+    def do_activate(self):
+        win = self.props.active_window
+        if not win:
+            win = MainWindow(application=self)
+        win.present()
+
+    def do_shutdown(self):
+        Gtk.Application.do_shutdown(self)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = Application()
+    app.run(sys.argv)
